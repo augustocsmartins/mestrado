8956be76b78ff7f550e79001fd6809f20a7e56c7
Augusto Cesar <augusto@base16.com.br>
2015-10-06 21:53:18 -0300

    inclusão da referência aos autores na qual essa implementação é baseada.

--- lib/coh-metrix-port/coh_metrix_port.rb
+++ lib/coh-metrix-port/coh_metrix_port.rb
@@ -17,14 +17,17 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Authors:  Augusto Cesar S. Martins <augusto@base16.com.br>
-
-# 26. Referências:
+#
+#* Referências:
 #* Dias-da Silva, B. C., Oliveira, M. F. d., and Moraes, H. R. d. (2002). Groundwork for the development of the brazilian portuguese wordnet. In PorTAL ’02: Proceedings of the Third International Conference on Advances in Natural Language Processing, pages 189–196, London, UK. Springer-Verlag.
 #* Dias-da Silva, B. C. (2003). Human language technology research and the development of the brazilian portuguese wordnet. 17th International Congress of Linguists.
 #* Dias-da Silva, B. C. (2005). A construç˜ao da base da wordnet.br: conquistas e desafios. In the Proceedings of the 3rd Workshop in Information and Human Language Technology (TIL’2005), pages 2238–2247.
 #* Dias-da Silva, B. C., Di Felippo, A., and Nunes, M. G. V. (2008). The automatic mapping of princeton wordnet lexical-conceptual relations onto the brazilian portuguese wordnet database. In Chair), N. C. C., Choukri, K., Maegaard, B., Mariani, J., Odjik, J., Piperidis, S., and Tapias, D., editors, Proceedings of the Sixth International Language Resources and Evaluation (LREC’08), Marrakech, Morocco. European Language Resources Association (ELRA). http://www.lrec-conf.org/proceedings/lrec2008/.
 #* Maziero, E. G., Pardo, T. A. S., Di Felipo, A. e Dias-da-Silva, B. C. (2008). A Base de Dados Lexical e a Interface Web do TeP 2.0 - Thesaurus Eletrônico para o Português do Brasil. Em Anais do VI Workshop em Tecnologia da Informação e da Linguagem Humana TIL, 2008, Vila Velha, ES.
 #* Scarton, C. E. e Aluísio, S. M. (2009). Herança Automática das Relações de Hiperonímia para a Wordnet.Br. Série de Relatórios do NILC. NILC-TR-09- 10, Dezembro, 48p.
+#* SCARTON, Carolina; GASPERIN, Caroline; ALUISIO, Sandra. Revisiting the readability assessment of texts in Portuguese. In: Advances in Artificial Intelligence–IBERAMIA 2010. Springer Berlin Heidelberg, 2010. p. 306-315.
+#* SCARTON, Carolina Evaristo; ALUÍSIO, Sandra Maria. Análise da Inteligibilidade de textos via ferramentas de Processamento de Língua Natural: adaptando as métricas do Coh-Metrix para o Português. Linguamática, v. 2, n. 1, p. 45-61, 2010.
+
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/coh-metrix-port/metrics')
 
 require 'metrics/ambiguity'
